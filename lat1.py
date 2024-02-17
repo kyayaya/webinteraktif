9ac794a6fb2edfb395d98c7acd7b80675502e9e4
kia <nurzakiyah.pakpahan11@gmail.com>
2024-02-16 22:58:35 -0800

    v1

---
+++ lat1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.DataFrame({
+    'No' : [1,2,3,4],
+    'Nama' : ['Nadia','Zahwa','Putri','Kia'],
+    'Nilai' : [89,90,100,94]
+})
+
+df
