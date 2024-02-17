825d5fdbb8be8e49cebaefa9e3dceebfccf51106
kia <nurzakiyah.pakpahan11@gmail.com>
2024-02-16 23:20:10 -0800

    v2

--- lat1.py
+++ lat1.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+
+st.write("KyayaPuput Tabel Coding")
 df = pd.DataFrame({
     'No' : [1,2,3,4],
     'Nama' : ['Nadia','Zahwa','Putri','Kia'],
-    'Nilai' : [89,90,100,94]
+    'Nilai' : [99,98,100,94]
 })
 
 df
