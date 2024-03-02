fc147e846802936470b6695dcc405e26b6ac3fab
kyayaya <nurzakiyah.pakpahan11@gmail.com>
2024-03-01 19:42:05 -0800

    r7

--- lat1.py
+++ lat1.py
@@ -1,5 +1,7 @@
 import streamlit as st
-#import pandas as pd
+from page1 import page_1
+from page2 import page_2
+from page3 import page_3
 #import matplotlib.pyplot as plt
 
 # st.write("Kyaya Tabel Coding")
@@ -11,22 +13,17 @@ import streamlit as st
 
 # df
 
-def page_1():
-    st.title("Halaman 1")
-    st.write("Halaman ini digunakan untuk Intro")
-def page_2():
-    st.title("Halaman 2")
-    st.write("Halaman ini digunakan untuk Menampilkan youtube")
-def page_3():
-    st.title("Halaman 3")
-    st.write("Halaman ini digunakan untuk Menampilkan Rumus Matematika")
+
+
+
+
 
 PAGES = {
     "Page 1" : page_1,
     "Page 2" : page_2,
     "Page 3" : page_3
 }
-st.sidebar.image("data.jpg", width=200)
+
 page = st.sidebar.radio("Halaman", list(PAGES.keys()))
 PAGES[page]()
 
---
+++ page2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+def page_2():
+    st.title("Halaman 2")
+    st.write("Halaman ini digunakan untuk Menampilkan youtube")
+    st.video("https://youtu.be/uMXdeSnaElQ?si=3KJh17dE2l3JvcnX")
